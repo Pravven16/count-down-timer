7e4fe8826c6862b643ca82ae0c009e1a0a60312b
pravven <pravven16@gmail.com>
2023-03-11 14:08:16 +0530

    count-down timer

---
+++ script.js
@@ -0,0 +1,34 @@
+const dayE1 = document.getElementById("days");
+const hoursE1 = document.getElementById("hours");
+const minsE1 = document.getElementById("mins");
+const secondsE1 = document.getElementById("seconds");
+
+const examDate = "16 Mar 2023";
+
+function countdown() {
+    const exam_date = new Date(examDate);
+    const currentDate =  new Date();
+
+    const totalSeconds = (exam_date - currentDate)/1000;
+    
+    const days = Math.floor(totalSeconds / 3600 /24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    dayE1.innerHTML =days;
+    hoursE1.innerHTML =formatTime(hours);
+    minsE1.innerHTML =formatTime(mins);
+    secondsE1.innerHTML =formatTime(seconds);
+
+}
+
+function formatTime(time) {
+    return time < 10 ?  `0${time}` : time;
+}
+
+//initial call
+
+countdown();
+
+setInterval(countdown,1000);
